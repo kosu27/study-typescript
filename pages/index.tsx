129a670cceb680e5074dfa6fcdd8b6c4f755ead7
kosu27 <kosumon27@gmail.com>
2021-11-10 15:20:06 +0900

    ✨ プリミティブ型について学んだ

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from "next";
 
-let foo = {} as { bar: number };
+const boo: boolean = true;
+const bar: number = 1;
+const baz: string = "hello";
+const qux: null = null;
 
-foo.bar = 1;
-
-function double(x: number): number | undefined {
-  if (x > 0) {
-    return;
-  }
-  return x * 2;
-}
+const foo: undefined = undefined;
+// nullは現在利用できない型→明示的に使えない状態を強く表したい時
+// undefinedは初期化されていない状態の型→暗黙的な状態を表したい時
+// 基本的にはnullはNG ＊jsonではundefinedは使えない→nullでないと使えない
 
 const Home: NextPage = () => {
   return <div>test</div>;
