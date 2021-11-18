8ab70863c1dce23b1d0fa02e7ac7412065bcd149
kosu27 <kosumon27@gmail.com>
2021-11-19 01:55:20 +0900

    ✨ unknown型について

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,10 @@
 import type { NextPage } from "next";
 
-const foo: any = "";
+const foo: unknown = "foo";
+
+if (typeof foo === "string") {
+  foo.substr(2);
+}
 
 const Home: NextPage = () => {
   return <div>test</div>;
