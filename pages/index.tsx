bfd725f17b37f523b59fa49634152b53739c87db
kosu27 <kosumon27@gmail.com>
2021-11-18 15:25:42 +0900

    ✨ ReactコンポーネントとLiteral Number型の書き方

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from "next";
 
-const foo: boolean = true;
+const boo: true = true;
+const str: "bar" = "bar";
+const num: 12 = 12;
+
+function double(x: 12): number {
+  return x * 2;
+}
+
+double(12);
 
 const Home: NextPage = () => {
+  return (
+    <div>
+      <Component foo />
+    </div>
+  );
+};
+
+const Component = (props: { foo?: true }) => {
+  if (props.foo) {
+    return <div>a</div>;
+  }
+
   return <div>test</div>;
 };
 
