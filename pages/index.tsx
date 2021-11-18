ae7acd24575d4e7e2a94653ad0b88f934798a666
kosu27 <kosumon27@gmail.com>
2021-11-19 01:48:05 +0900

    ✨ Tuple型定義

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
 import type { NextPage } from "next";
 
-const foo: (number | string | boolean)[] = [1, 2, "3", true];
+const foo: [string, number] = ["foo", 0];
+
+foo[1].toFixed(2);
 
 const Home: NextPage = () => {
   return <div>test</div>;
