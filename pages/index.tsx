d31d70d3376a5f97e4c9e8220c251fa1b71e6b97
kosu27 <kosumon27@gmail.com>
2021-11-19 01:50:56 +0900

    ✨ any型について

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import type { NextPage } from "next";
 
-const foo: [string, number] = ["foo", 0];
-
-foo[1].toFixed(2);
+const foo: any = "";
 
 const Home: NextPage = () => {
   return <div>test</div>;
